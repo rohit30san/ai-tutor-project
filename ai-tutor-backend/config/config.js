f31a50ec43475b1dc7a2f4e55426925cb3075064
Rohit Sangwan <rohit30san@gmail.com>
2025-06-17 15:59:46 +0530

    updated db

--- ai-tutor-backend/config/config.js
+++ ai-tutor-backend/config/config.js
@@ -7,5 +7,12 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false }
+    }
   }
 };
